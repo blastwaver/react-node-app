df8a0be15055834f37026c4308b73a5e58e5121b
Kyeongho Na <blastwaver.k@gmail.com>
2017-11-09 15:21:58 -0800

    'OathBasic'

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./.config/key');
+
+
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send({bye:'body!'});
-});
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done ) => {
+        console.log('accessToken: ', accessToken);
+        console.log('refreshToken: ', refreshToken);
+        console.log('profile: ', accessToken); 
+    })
+); 
+
+app.get('/auth/google', passport.authenticate('google',{
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
